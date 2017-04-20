23ce6e951a0e67dc3d5bd657c9ee7d2fbcb32575
Jack Moore <jmoore6786wjm@hotmail.com>
2017-04-20 19:48:16 -0400

    Migration for widgets correct now

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,9 @@ ActiveRecord::Schema.define(version: 20170420230237) do
   create_table "widgets", force: :cascade do |t|
     t.string   "title"
     t.text     "attributes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "widgetpicture"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
