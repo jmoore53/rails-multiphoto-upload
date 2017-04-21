e9527be9de0d19e90241a2745e2e9349ae63eeba
Jack Moore <jmoore6786wjm@hotmail.com>
2017-04-20 20:18:04 -0400

    Controller updated

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,14 +1,24 @@
 class WidgetsController < ApplicationController
     def index
+        @widgets = Widget.all
     end
     def new
         @widget = Widget.new
     end
     def create
+        @widget = Widget.new(widget_params)
+        if @widget.save
+            redirect_to @widget
+        else
+            render "new"
+        end
     end
     def edit
     end
     def update
+    end
+    def show
+        @widget = Widget.find(params[:id])
     end
     private
     def widget_params
