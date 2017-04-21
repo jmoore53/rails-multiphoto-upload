0faeb7dc7603e254be4f54ba0317a8ed2ec9d1eb
Jack Moore <jmoore6786wjm@hotmail.com>
2017-04-20 22:53:19 -0400

    Can post multiple pictures and comments to the website

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module MultiphotoUpload
   class Application < Rails::Application
     require 'carrierwave'
+    require 'carrierwave/orm/activerecord'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
