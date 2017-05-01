8d1ae85544a5a5f777bfc326d85815562700bddd
Jack Moore <jmoore6786wjm@hotmail.com>
2017-05-01 02:03:00 -0400

    Controller update and edit actions added

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -14,13 +14,25 @@ class WidgetsController < ApplicationController
         end
     end
     def edit
+        @widget = Widget.find(params[:id])
     end
     def update
+        @widget = Widget.find(params[:id])
+        if @widget.update(widget_params)
+            redirect_to @widget
+        else
+            render @edit
+        end
     end
     def show
         @widget = Widget.find(params[:id])
     end
     private
+    def add_more_images(new_images)
+        images = @widget.widgetpictures
+        images += new_images
+        @widget.widgetpictures = images
+    end
     def widget_params
         params.require(:widget).permit(:user_id,{widgetAttributes: []}, {widgetpictures: []})
     end
